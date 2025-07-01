0f635223cfd6acc73b12f7368c069c7de000da2d
Abhinav Pandey <abhinavpandey1230@gmail.com>
2025-07-02 01:23:42 +0530

    Update clerk.go

--- server/middleware/clerk.go
+++ server/middleware/clerk.go
@@ -66,6 +66,7 @@ func ClerkMiddleware() fiber.Handler {
 				allowed := []string{
 					"http://localhost:3000",
 					"http://127.0.0.1:3000",
+					"https://klara-ai.vercel.app/",
 				}
 				for _, a := range allowed {
 					if azp == a {
